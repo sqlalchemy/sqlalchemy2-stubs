5d572a16b095ff617d48d5458054319274176fa3
Mike Bayer <mike_mp@zzzcomputing.com>
2021-03-01 09:28:14 -0500

    use old setup style for now

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+import os
+
 from setuptools import setup
 
-setup()
+
+def find_stub_files():
+    result = []
+    for root, dirs, files in os.walk("sqlalchemy-stubs"):
+        for file in files:
+            if file.endswith(".pyi"):
+                if os.path.sep in root:
+                    sub_root = root.split(os.path.sep, 1)[-1]
+                    file = os.path.join(sub_root, file)
+                result.append(file)
+    return result
+
+
+setup(
+    packages=["sqlalchemy-stubs"],
+    package_data={"sqlalchemy-stubs": find_stub_files()},
+)
