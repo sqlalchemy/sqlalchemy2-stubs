768d7c8557e533f63dc2b8ebe8b27045d4663f8a
Mike Bayer <mike_mp@zzzcomputing.com>
2021-03-10 16:03:37 -0500

    dev

--- sqlalchemy-stubs/engine/create.pyi
+++ sqlalchemy-stubs/engine/create.pyi
@@ -1,13 +1,16 @@
 from typing import Any
+from typing import Union
 
 from . import base as base
+from .base import Engine
 from .mock import create_mock_engine as create_mock_engine
+from .url import URL
 from .. import event as event
 from .. import exc as exc
 from .. import util as util
 from ..sql import compiler as compiler
 
-def create_engine(url: Any, **kwargs: Any): ...
+def create_engine(url: Union[str, URL], **kwargs: Any) -> Engine: ...
 def engine_from_config(
     configuration: Any, prefix: str = ..., **kwargs: Any
-): ...
+) -> Engine: ...
