07c9e39e859dfb92241705d5e27e092049eb41c9
Bryan Forbes <bryan@reigndropsfall.net>
2021-04-14 17:39:28 -0500

    Improve `ext.declarative`

--- sqlalchemy-stubs/ext/declarative/__init__.pyi
+++ sqlalchemy-stubs/ext/declarative/__init__.pyi
@@ -1,13 +1,10 @@
-from typing import Any
-
 from .extensions import AbstractConcreteBase as AbstractConcreteBase
 from .extensions import ConcreteBase as ConcreteBase
 from .extensions import DeferredReflection as DeferredReflection
 from .extensions import instrument_declarative as instrument_declarative
+from ...orm.decl_api import as_declarative as as_declarative
+from ...orm.decl_api import declarative_base as declarative_base
 from ...orm.decl_api import DeclarativeMeta as DeclarativeMeta
 from ...orm.decl_api import declared_attr as declared_attr
-
-def declarative_base(*arg: Any, **kw: Any): ...
-def as_declarative(*arg: Any, **kw: Any): ...
-def has_inherited_table(*arg: Any, **kw: Any): ...
-def synonym_for(*arg: Any, **kw: Any): ...
+from ...orm.decl_api import has_inherited_table as has_inherited_table
+from ...orm.decl_api import synonym_for as synonym_for
--- sqlalchemy-stubs/ext/declarative/extensions.pyi
+++ sqlalchemy-stubs/ext/declarative/extensions.pyi
@@ -1,14 +1,9 @@
 from typing import Any
+from typing import Type
 
-from ... import inspection as inspection
-from ... import util as util
-from ...orm import registry as registry
-from ...orm import relationships as relationships
-from ...orm.util import polymorphic_union as polymorphic_union
-from ...schema import Table as Table
-from ...util import OrderedDict as OrderedDict
-
-def instrument_declarative(cls, cls_registry: Any, metadata: Any): ...
+def instrument_declarative(
+    cls: Type[Any], cls_registry: Any, metadata: Any
+) -> Any: ...
 
 class ConcreteBase:
     @classmethod
