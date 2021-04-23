0dc6cdeba0b6cf23609524e9b5e16f0bfccbdbc3
Federico Caselli <cfederico87@gmail.com>
2021-04-23 22:52:09 +0200

    fix pipeline

--- .github/workflows/scripts/find_version.py
+++ .github/workflows/scripts/find_version.py
@@ -4,16 +4,16 @@ from urllib import request
 
 
 def go(argv):
-    if "sqla_release" not in argv:
-        print('master')
-    result = request.urlopen(
-        "https://pypi.org/pypi/SQLAlchemy/json", timeout=10
-    )
-    assert result.status == 200
-    parsed = json.loads(result.read())
-    version = parsed["info"]["version"]
-    print(f"rel_{version}".replace(".", "_"))
-
+    if "sqla_release" in argv:
+        result = request.urlopen(
+            "https://pypi.org/pypi/SQLAlchemy/json", timeout=10
+        )
+        assert result.status == 200
+        parsed = json.loads(result.read())
+        version = parsed["info"]["version"]
+        print(f"rel_{version}".replace(".", "_"))
+    else:
+        print("master")
 
 if __name__ == "__main__":
     go(sys.argv)
