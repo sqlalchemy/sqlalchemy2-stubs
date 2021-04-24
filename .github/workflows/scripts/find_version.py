97986c307d826f34c2373759296a4bc5fee28514
Federico Caselli <CaselIT@users.noreply.github.com>
2021-04-24 13:14:09 +0200

    Add test workflox and tox (#74)
    * add tests
    
    * fix pipeline
    
    * Update .github/workflows/run-test.yaml
    
    Co-authored-by: Ash Berlin-Taylor <ash_github@firemirror.com>
    
    Co-authored-by: Ash Berlin-Taylor <ash_github@firemirror.com>

---
+++ .github/workflows/scripts/find_version.py
@@ -0,0 +1,19 @@
+import json
+import sys
+from urllib import request
+
+
+def go(argv):
+    if "sqla_release" in argv:
+        result = request.urlopen(
+            "https://pypi.org/pypi/SQLAlchemy/json", timeout=10
+        )
+        assert result.status == 200
+        parsed = json.loads(result.read())
+        version = parsed["info"]["version"]
+        print(f"rel_{version}".replace(".", "_"))
+    else:
+        print("master")
+
+if __name__ == "__main__":
+    go(sys.argv)
