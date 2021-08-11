35549d8ab8d7a13f3f53f2c4caf72ed43a3426c4
Anders Kaseorg <andersk@mit.edu>
2021-08-10 21:33:32 -0700

    Fix annotations for Pool.recreate, Pool.status
    Fixes #146.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

---
+++ test/files/pool_recreate_status.py
@@ -0,0 +1,5 @@
+import sqlalchemy, sqlalchemy.pool
+
+engine = sqlalchemy.create_engine("postgresql://scott:tiger@localhost/test")
+status: str = engine.pool.status()
+other_pool: sqlalchemy.pool.Pool = engine.pool.recreate()
