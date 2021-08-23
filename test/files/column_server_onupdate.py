527dd3be6768fa038bf3ab09d7d3e00212f549a2
Federico Caselli <CaselIT@users.noreply.github.com>
2021-08-23 22:17:28 +0200

    server_onupdate cannot be a function. improve tests (#160)

--- test/files/column_server_onupdate.py
+++ test/files/column_server_onupdate.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column
+from sqlalchemy import FetchedValue
 from sqlalchemy.orm import registry
-from sqlalchemy.sql import functions as func
 from sqlalchemy.types import Integer
 
 reg: registry = registry()
@@ -11,10 +11,10 @@ class ColumnServerOnUpdateArgument:
     __tablename__ = "onupdate"
 
     # overload 1: (__name: str, *args, ...)
-    overload1: int = Column("name", server_onupdate=func.now(), nullable=False)
+    overload1: int = Column("name", server_onupdate=FetchedValue(), nullable=False)
     # overload 2: (*args, ...)
-    Column(server_onupdate=func.now(), nullable=False)
+    Column(server_onupdate=FetchedValue(), nullable=False)
     # overload 3: (__name: str, __type: _TE, *args, ...)
-    overload3 = Column("name", Integer, server_onupdate=func.now(), nullable=False)
+    overload3 = Column("name", Integer, server_onupdate=FetchedValue(), nullable=False)
     # overload 4: (__type: _TE, *args, ...)
-    overload4 = Column(Integer, server_onupdate=func.now(), nullable=False)
+    overload4 = Column(Integer, server_onupdate=FetchedValue(), nullable=False)
