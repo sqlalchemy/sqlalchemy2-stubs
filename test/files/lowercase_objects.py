529a540c3de067cde4281028ebc43ccf79b506fe
Federico Caselli <cfederico87@gmail.com>
2021-08-24 22:28:37 +0200

    Improve types of lowecase table, column and values
    fixes #161

---
+++ test/files/lowercase_objects.py
@@ -0,0 +1,13 @@
+import sqlalchemy as sa
+
+Book = sa.table(
+    "book",
+    sa.column("id", sa.Integer),
+    sa.column("name", sa.String),
+)
+Book.append_column(sa.column("other"))
+Book.corresponding_column(Book.c.id)
+
+value_expr = sa.values(
+    sa.column("id", sa.Integer), sa.column("name", sa.String), name="my_values"
+).data([(1, "name1"), (2, "name2"), (3, "name3")])
