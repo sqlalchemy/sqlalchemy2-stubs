18e640bf317a1828256b0c11fd7afa835dc4267e
[Partner] Shota Sivsivadze <sivsivadze.shota.b@gmail.com>
2022-04-19 18:06:24 +0400

    212-Add typings

--- sqlalchemy-stubs/dialects/postgresql/dml.pyi
+++ sqlalchemy-stubs/dialects/postgresql/dml.pyi
@@ -1,28 +1,32 @@
-from typing import Any
-from typing import Optional
+from typing import Any, Union, Sequence, Optional, Mapping
 
-from ... import util as util
+from . import ExcludeConstraint
+from ... import util as util, Column, Index, Constraint
 from ...sql.dml import Insert as StandardInsert
-from ...sql.elements import ClauseElement
+from ...sql.elements import ClauseElement, ColumnElement
+from ...sql.functions import GenericFunction
+
 
 class Insert(StandardInsert):
     stringify_dialect: str = ...
     @util.memoized_property
     def excluded(self): ...
+
     def on_conflict_do_update(
-        self,
-        constraint: Optional[Any] = ...,
-        index_elements: Optional[Any] = ...,
-        index_where: Optional[Any] = ...,
-        set_: Optional[Any] = ...,
-        where: Optional[Any] = ...,
-    ) -> None: ...
+            self,
+            constraint: Optional[Union[str, Index, Constraint, ExcludeConstraint]] = ...,
+            index_elements: Sequence[Union[str, Column]] = ...,
+            index_where: Optional[ClauseElement] = ...,
+            set_: Mapping[str, Union[ColumnElement, GenericFunction]] = ...,
+            where: Optional[ClauseElement] = ...,
+    ) -> 'Insert': ...
+
     def on_conflict_do_nothing(
-        self,
-        constraint: Optional[Any] = ...,
-        index_elements: Optional[Any] = ...,
-        index_where: Optional[Any] = ...,
-    ) -> None: ...
+            self,
+            constraint: Optional[Union[str, Index, Constraint, ExcludeConstraint]] = ...,
+            index_elements: Optional[Sequence[Union[str, Column]]] = ...,
+            index_where: Optional[Any] = ...,
+    ) -> 'Index': ...
 
 insert: Any
 
@@ -33,8 +37,8 @@ class OnConflictClause(ClauseElement):
     inferred_target_whereclause: Any = ...
     def __init__(
         self,
-        constraint: Optional[Any] = ...,
-        index_elements: Optional[Any] = ...,
+        constraint: Optional[Union[str]] = ...,
+        index_elements: Sequence[Union[str, Column]] = ...,
         index_where: Optional[Any] = ...,
     ) -> None: ...
 
@@ -47,9 +51,9 @@ class OnConflictDoUpdate(OnConflictClause):
     update_whereclause: Any = ...
     def __init__(
         self,
-        constraint: Optional[Any] = ...,
-        index_elements: Optional[Any] = ...,
+        constraint: Optional[Union[str, Index, Constraint, ExcludeConstraint]] = ...,
+        index_elements: Optional[Sequence[Union[str, Column]]] = ...,
         index_where: Optional[Any] = ...,
-        set_: Optional[Any] = ...,
-        where: Optional[Any] = ...,
+        set_: Mapping[str, Union[ColumnElement, GenericFunction]] = ...,
+        where: Optional[ClauseElement] = ...,
     ) -> None: ...
